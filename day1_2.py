fa1eb69bd1ded60e517b70398d9a1dcff5d4e79b
Roman92P <roman.paszkow@gmail.com>
2023-12-04 00:26:58 +0100

    commit

---
+++ day1_2.py
@@ -0,0 +1,25 @@
+import re
+
+filename = "day1_2a.txt"
+
+nums = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+]
+
+with open(filename) as f:
+    content = f.read().split()
+
+result = 0
+
+for line in content:
+
+print(result)
